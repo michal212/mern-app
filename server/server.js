3b3d03db943b2c5aff30729f0ade2b070167ed63
michal212 <michalfarada1997@gmail.com>
2021-08-09 16:43:26 +0300

    secondcommit

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path =require('path')
 app.use(cors());
 app.get("/", (req, res) => {
   res.send("hello from srerver");
@@ -9,3 +10,10 @@ app.get("/", (req, res) => {
 app.listen(8080, (req, res) => {
   console.log("port running on port 8080 ");
 });
+
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static(path.join(__dirname,'../client/bulid')));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../client/bulid,'index.html"))
+    })
+}
