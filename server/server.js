e559d5e01e628f37b54a0a3ab71ea96d1f8823ec
michal212 <michalfarada1997@gmail.com>
2021-08-10 11:29:48 +0300

    app.get

--- server/server.js
+++ server/server.js
@@ -8,15 +8,14 @@ const app = express();
 const path = require("path");
 app.use(cors());
 
-
 app.listen(process.env.PORT || 8080, (req, res) => {
   console.log("port running on port 8080 ");
 });
 app.use("/api/student", studentRouter);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/bulid")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/bulid",'index.html'));
-  });
-}
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.get('*', (req, res)=>{
+        res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
+    });
+  }
