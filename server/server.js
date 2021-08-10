7a76d26cdfdb3a45405e3b417178d906bc2bd793
michal212 <michalfarada1997@gmail.com>
2021-08-10 12:35:22 +0300

    d

--- server/server.js
+++ server/server.js
@@ -13,9 +13,9 @@ app.listen(process.env.PORT || 8080, (req, res) => {
 });
 
 app.use("/api/student", studentRouter);
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
   });
 }
