e1b28bf5aad22f2b88800d78b4e4179d6faddd01
michal212 <michalfarada1997@gmail.com>
2021-08-10 10:24:03 +0300

    a

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const cors = require("cors");
 const express = require("express");
 const app = express();
-const cors = require("cors");
-const path =require('path')
+const path = require("path");
 app.use(cors());
 // app.get("/", (req, res) => {
 //   res.send("hello from srerver");
@@ -11,9 +13,9 @@ app.listen(8080, (req, res) => {
   console.log("port running on port 8080 ");
 });
 
-if(process.env.NODE_ENV==='production'){
-    app.use(express.static(path.join(__dirname,'../client/bulid')));
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../client/bulid,'index.html"))
-    })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/bulid")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/bulid,'index.html"));
+  });
 }
